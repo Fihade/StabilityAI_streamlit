379fc34f23cea63a42b055445e2734f745ec5bc6
Miller Liang <841985944@qq.com>
2023-05-16 19:06:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -49,13 +49,13 @@ with col2:
 	height = st.number_input("height", value=512)
 
 if st.button('Say hello'):
-#	print(
-#		f"""
-#		host: {STABILITY_HOST}\n key: {STABILITY_KEY}\n prompt: {prompt}\n negative: {negative_prompt}\n sampler: {sampler}\n seed: {seed}\n scale={scale}\n  count: {count}\n  steps: {steps}\n width: {width}\n height: {height}\n
-#		""")
-#	st.write(f"""
-#		host: {STABILITY_HOST}\n key: {STABILITY_KEY}\n prompt: {prompt}\n negative: {negative_prompt}\n sampler: {sampler}\n seed: {seed}\n scale={scale}\n  count: {count}\n  steps: {steps}\n width: {width}\n height: {height}\n
-#		""")
+	print(
+		f"""
+		host: {STABILITY_HOST}\n key: {STABILITY_KEY}\n prompt: {prompt}\n negative: {negative_prompt}\n sampler: {sampler}\n seed: {seed}\n scale={scale}\n  count: {count}\n  steps: {steps}\n width: {width}\n height: {height}\n
+		""")
+	st.write(f"""
+		host: {STABILITY_HOST}\n key: {STABILITY_KEY}\n prompt: {prompt}\n negative: {negative_prompt}\n sampler: {sampler}\n seed: {seed}\n scale={scale}\n  count: {count}\n  steps: {steps}\n width: {width}\n height: {height}\n
+		""")
 	stability_api = client.StabilityInference(
 		host=STABILITY_HOST,
 		key=STABILITY_KEY, # API Key reference.
@@ -64,17 +64,17 @@ if st.button('Say hello'):
 	)
 	
 	answers = stability_api.generate(
-		prompt=prompt,
+		prompt="expansive landscape rolling greens with blue daisies and weeping willow trees under a blue alien sky, artstation, masterful, ghibli",
 		seed=992446758, # If a seed is provided, the resulting generated image will be deterministic.
 		# What this means is that as long as all generation parameters remain the same, you can always recall the same image simply by generating it again.
 		# Note: This isn't quite the case for Clip Guided generations, which we'll tackle in a future example notebook.
-		steps=steps, # Amount of inference steps performed on image generation. Defaults to 30. 
-		cfg_scale=scale, # Influences how strongly your generation is guided to match your prompt.
+		steps=30, # Amount of inference steps performed on image generation. Defaults to 30. 
+		cfg_scale=8.0, # Influences how strongly your generation is guided to match your prompt.
 		# Setting this value higher increases the strength in which it tries to match your prompt.
 		# Defaults to 7.0 if not specified.
-		width=width, # Generation width, defaults to 512 if not included.
-		height=height, # Generation height, defaults to 512 if not included.
-		samples=count, # Number of images to generate, defaults to 1 if not included.
+		width=512, # Generation width, defaults to 512 if not included.
+		height=512, # Generation height, defaults to 512 if not included.
+		samples=1, # Number of images to generate, defaults to 1 if not included.
 		sampler=generation.SAMPLER_K_DPMPP_2M # Choose which sampler we want to denoise our generation with.
 		# Defaults to k_dpmpp_2m if not specified. Clip Guidance only supports ancestral samplers.
 		# (Available Samplers: ddim, plms, k_euler, k_euler_ancestral, k_heun, k_dpm_2, k_dpm_2_ancestral, k_dpmpp_2s_ancestral, k_lms, k_dpmpp_2m)
